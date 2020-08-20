746166afa21503691af4238bfa8eb40dbd761414
Sidak <sidak.pasrichassp@gmail.com>
2020-08-20 23:44:08 +0530

    Task 3: Design Entities To Represent Your Data

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,84 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.user.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private PetType type;
+    private String name;
+
+    @ManyToOne
+    private User owner;
+
+    private LocalDate birthDate;
+    private String notes;
+
+    public Pet() {
+    }
+
+    public Pet(Long id, PetType type, String name, User owner, LocalDate birthDate, String notes) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
